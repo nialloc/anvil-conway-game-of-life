98e787eba3583b8a332bed737c704902331d6bc6
niall@tigduv.com <niall@tigduv.com>
2022-05-05 21:00:24 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,11 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+import random
 
 class Form1(Form1Template):
 
@@ -8,3 +14,43 @@ class Form1(Form1Template):
         self.init_components(**properties)
 
         # Any code you write here will run when the form opens.
+        self.rows = 32
+        self.cols = 32
+        self.width = 20
+        self.height = 20
+        self.reset_board()
+        
+    def show_board(self):
+        c = self.canvas_1
+#         c.width = 800
+        c.height = 160
+#         c.background = "#FF0000"
+
+        for index,value in enumerate(self.board):
+
+            col = int(index % self.cols)
+            row = int(index / self.cols)
+            
+            x = col * self.width
+            y = row * self.width
+            width = self.width
+            height = self.height
+            if value == 1:
+                c.fill_rect(x, y, width, height)
+            else:
+                c.clear_rect(x, y, width, height)
+    
+    def button_2_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        self.show_board()
+
+    def button_1_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        self.reset_board()
+        self.show_board()
+        
+    def reset_board(self):
+        self.board = [int(round(random.random())) for x in range(self.rows*self.cols)]
+       
+
+
