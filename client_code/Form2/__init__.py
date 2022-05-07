09cc54e76756b76d3d992dbbc2431570f33039a9
niall@tigduv.com <niall@tigduv.com>
2022-05-07 19:40:07 +0000

    Edited Form2

--- client_code/Form2/__init__.py
+++ client_code/Form2/__init__.py
@@ -5,15 +5,43 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-import anvil.js
-
-
-
 class Form2(Form2Template):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
 
-        data = anvil.server.call('get_data')
-        print('data',data)
+        self.cols = 32
+        self.rows = 32
+        self.width = 16
+        self.height = 16
+        self.board = [0 for x in range(self.cols*self.rows)]
+        print('ll')
            
+    def button_get_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        data = anvil.server.call('get_data')
+        self.board = data
+        self.show_board()
+        
+    def show_board(self):
+        c = self.canvas_1
+#         c.background = "#FFF0F0"
+
+        for index,value in enumerate(self.board):
+
+            col = int(index % self.cols)
+            row = int(index / self.cols)
+            
+            x = col * self.width
+            y = row * self.width
+            width = self.width
+            height = self.height
+            if value == 1:
+#                 c.fill_style("#FF0000")
+                c.fill_rect(x, y, width, height)
+            else:
+#                 c.fill_style("#FFFFFF")
+                c.clear_rect(x, y, width, height)
+        
+
+
